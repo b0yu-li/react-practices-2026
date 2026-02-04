defa7b7ca2a627f7a71a64d440b3ceb6e8fcb526
Boyu Li <lby1251853265@icloud.com>
2026-02-04 16:32:31 +0800

    refactor: move StarRating to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import { StarRating } from './components/StarRating'
 
 function App() {
   const [currentRating, setCurrentRating] = useState(5)
@@ -14,6 +13,42 @@ function App() {
           setCurrentRating(newRating)
         }}></StarRating>
     </>
+  )
+}
+
+interface StarRatingProps {
+  maxStars: number,
+  currentRating: number,
+  onChange: (newRating: number) => void
+}
+
+const StarRating = ({ maxStars, currentRating, onChange }: StarRatingProps) => {
+
+  const handleClick = (index: number) => {
+    console.debug(`Star no. ${index} clicked`);
+    onChange(index);
+  };
+
+  const stars = [];
+  for (let index = 0; index < maxStars; index++) {
+    let starSymbol = '☆'
+    if (index <= currentRating) {
+      starSymbol = '★'
+    }
+    const star = <span
+      key={`${index}`}
+      onClick={() => { handleClick(index) }}>{starSymbol}
+    </span>
+    stars.push(star)
+  }
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <span>How would you rate it?</span>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        {stars}
+      </div>
+    </div>
   )
 }
 
