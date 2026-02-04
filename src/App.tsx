f7ff38bf4baa53e3dffda2047f033c1a782a8fcd
Boyu Li <lby1251853265@icloud.com>
2026-02-04 17:05:47 +0800

    refactor: Move from Imperative to Declarative

--- src/App.tsx
+++ src/App.tsx
@@ -29,25 +29,22 @@ const StarRating = ({ maxStars = 5, currentRating, onChange }: StarRatingProps) 
     onChange(index);
   };
 
-  const stars = [];
-  for (let index = 1; index <= maxStars; index++) {
-    let starSymbol = '☆'
-    if (index <= currentRating) {
-      starSymbol = '★'
-    }
-    const star = <span
-      style={{ cursor: 'pointer', fontSize: '24px' }}
-      key={`${index}`}
-      onClick={() => { handleClick(index) }}>{starSymbol}
-    </span>
-    stars.push(star)
-  }
+  const buildStars = (() => {
+    const indices = Array.from({ length: maxStars }, (_, k) => { return k + 1 })
+    return indices.map(index => {
+      return <span
+        style={{ cursor: 'pointer', fontSize: '24px' }}
+        key={`${index}`}
+        onClick={() => { handleClick(index) }}>{index <= currentRating ? '★' : '☆'}
+      </span>
+    })
+  })
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <span>How would you rate it?</span>
       <div style={{ display: "flex", flexDirection: "row", gap: 3 }}>
-        {stars}
+        {buildStars()}
       </div>
     </div>
   )
